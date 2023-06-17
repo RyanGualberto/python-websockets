1b2c65d5efb5e300626a032a32d0bd1db4571a7c
ryangualberto <ryanoliveiragualberto@gmail.com>
2023-06-17 10:19:07 -0300

    feat: turn dynamic the message in level 1

--- level_1/client.py
+++ level_1/client.py
@@ -1,11 +1,13 @@
 # importa o módulo de conexão do websocket
 from websockets.sync.client import connect
 
+message_to_send = input("Type a message: ")
+
 def main():
     # Faz uma tentativa de conexão com sebsocket server na url 
     with connect("ws://localhost:8765") as websocket:
         # envia a mensagem para o sevidor
-        websocket.send("Hello world!")
+        websocket.send(message=message_to_send)
         # recebe uma mensagem do servidor
         message = websocket.recv()
         # exibe a mensagem do servidor
